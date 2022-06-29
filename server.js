35b554a554decc586ed1d353e2b865f7027236d3
AnmolPlayzz <anmolisnotavailable@gmail.com>
2022-06-29 08:30:19 +0000

    Oopsy

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ server.all('/', (req, res)=>{
     res.send('Your bot is alive!')
 })
 function keepAlive(){
-    server.listen(3000, ()=>{console.log("Server is Ready!")});
+    server.listen(8080, ()=>{console.log("Server is Ready!")});
 }
 module.exports = keepAlive;
