08e113f2b541fca60b23bf71de120a2d17de3e3b
AnmolPlayzz <anmolisnotavailable@gmail.com>
2022-06-29 14:30:06 +0000

    Fix: broken command (deployments and runners)

--- deploy-commands.js
+++ deploy-commands.js
@@ -4,13 +4,22 @@ const { token, clientId, guildId } = require('./config.json');
 const fs = require('node:fs');
 
 const commands = [];
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commandFolder = fs.readdirSync('./commands');
+
 
 // Place your client and guild ids here
+for (const folder of commandFolder) {
+	const commandFiles = fs.readdirSync(`./commands/${folder}/`).filter(file => file.endsWith('.js'));
+	//convert to an object
+	const commandObject = {};
+	
+	for (const file of commandFiles) {
 
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	commands.push(command.data.toJSON());
+		const command = require(`./commands/${folder}/${file}`);
+		commandObject[command.data] = command;
+		console.log(command.data);
+		commands.push(command.data);
+	}
 }
 
 const rest = new REST({ version: '9' }).setToken(token);
